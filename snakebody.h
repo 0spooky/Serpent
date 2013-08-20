101f1fce1d63f4c8e7a17574bad628705731a0b1
Futochu <kevin_c@telus.net>
2013-08-20 09:12:28 -0700

    Create snakebody.h

---
+++ snakebody.h
@@ -0,0 +1,27 @@
+#ifndef SNAKEBODY_H_INCLUDED
+#define SNAKEBODY_H_INCLUDED
+#include <SFML/graphics.hpp>
+#include <vector>
+
+extern int gamemap[50][40];
+
+class snakebody{
+public:
+    int partposx;
+    int partposy;
+    sf::Sprite body;
+    snakebody();
+    snakebody(float partposx, float partposy);
+};
+
+class snakebodymanager{
+public:
+    bool notlosing;
+    int bodycount;
+    std::vector<snakebody> totalbody;
+    void bodygrow();
+    int headmovedirection(int direction);
+    snakebodymanager();
+};
+
+#endif // SNAKEBODY_H_INCLUDED
